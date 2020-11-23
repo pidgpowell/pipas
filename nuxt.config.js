a3a29726e902787c03b1f3f9ec3c1c90e3ac5a87
Mark Powell <mpowell@atomicsoftware.com>
2020-11-23 09:51:38 -0500

    nuxt config updates

--- nuxt.config.js
+++ nuxt.config.js
@@ -5,10 +5,13 @@ export default {
   // Global page headers (https://go.nuxtjs.dev/config-head)
   head: {
     title: 'Pipas',
+    htmlAttrs: {
+      lang: 'en',
+    },
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
-      { hid: 'description', name: 'description', content: '' }
+      { hid: 'description', name: 'description', content: 'pipas (the band) - anti-trump --> pro-prosecution.' }
     ],
     link: [
       { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
