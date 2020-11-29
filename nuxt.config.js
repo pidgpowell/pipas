f60c04732bb351c1263c4ccac50d60ac171dd665
Mark Powell <mpowell@atomicsoftware.com>
2020-11-29 13:59:47 -0500

    config update

--- nuxt.config.js
+++ nuxt.config.js
@@ -11,7 +11,7 @@ export default {
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
-      { hid: 'description', name: 'description', content: 'pipas (the band) - anti-trump --> pro-prosecution.' }
+      { hid: 'description', name: 'description', content: 'Pipas (the band) - anti-Trump --> pro-prosecution.' }
     ],
     link: [
       { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
