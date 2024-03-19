7fe907f89b8a24a0d6321fba883682fa38d6f9a7
Mark Powell <longlostcousin@gmail.com>
2024-03-19 12:22:13 -0400

    tour dates updates

--- src/utils/dateFormat.ts
+++ src/utils/dateFormat.ts
@@ -1,18 +1,12 @@
-function addHours(date: string | number | Date, hours: number) {
-  const newDate = new Date(date);
-  newDate.setHours(newDate.getHours() + hours);
-  return newDate;
-}
-
 export default function (date: string | Date, options?: Intl.DateTimeFormatOptions) {
-    const dateFormatOptions: Intl.DateTimeFormatOptions = {
-      year: 'numeric',
-      day: 'numeric',
-      month: 'short',
-      timeZone: 'UTC',
-      ...options,
-    }
+  const dateFormatOptions: Intl.DateTimeFormatOptions = {
+    year: 'numeric',
+    day: 'numeric',
+    month: 'short',
+    timeZone: 'UTC',
+    ...options,
+  }
 
-    if (!Date.parse(date as string)) return;
-    return Intl.DateTimeFormat("en-GB", dateFormatOptions).format(new Date(date));
+  if (!Date.parse(date as string)) return;
+  return Intl.DateTimeFormat("en-GB", dateFormatOptions).format(new Date(date));
 }
